a6a181cef9f7f6f5b99357e837312cae651ee17b
Salman <sfariscc@gmail.com>
2022-08-31 15:14:44 +0530

    question 5 added

--- 4.js
+++ 4.js
@@ -1,3 +1,3 @@
-const arr = [1, 2, -3];
+const arr = [1, 2, -3,15,0,8,-100];
 const max = Math.max(...arr);
 console.log(max)
---
+++ 5.js
@@ -0,0 +1,3 @@
+const points = [40, 100, 1, 5, 25, 10];
+const data = points.sort(function(a, b){return b-a});
+console.log(data)
